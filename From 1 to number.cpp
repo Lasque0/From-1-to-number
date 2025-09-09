af7d8f9636e53b400d74e4ba0c621636b2c19be1
Lasque <lasquee0@gmail.com>
2025-09-10 01:22:21 +0300

    Add files via upload

---
+++ From 1 to number.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int x,i;
+    cout<<"Enter a number: ";
+    cin>>x;
+
+    for(i=1;i<=x;i++){ //From 1 to number
+        cout<<"Your Message: " << i << "\n";
+    }
+
+     return 0;
+
+}
